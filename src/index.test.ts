29d53038b6bf6c2568fc07337eb4b330299443ad
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2026-02-13 15:20:26 -0500

    Add num parser

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,4 @@
-import { run, sequenceOf, str } from './';
+import { num, run, sequenceOf, str } from './';
 
 describe('str', () => {
   it('should match a string', () => {
@@ -11,29 +11,72 @@ describe('str', () => {
       original: 'Hello world!',
     });
 
-    expect(run(parser, 'Goodbye world!')).toMatchObject({
-      success: false,
-      error: expect.stringContaining(`"Goodbye worl"`),
-      original: 'Goodbye world!',
+    expect(run(parser, 'Goodbye world!')).toMatchInlineSnapshot(`
+     {
+       "error": "Expected "Hello world!", but received "Goodbye worl"",
+       "original": "Goodbye world!",
+       "success": false,
+     }
+    `);
+
+    expect(run(parser, '')).toMatchInlineSnapshot(`
+     {
+       "error": "Expected "Hello world!", but received "<end of input>"",
+       "original": "",
+       "success": false,
+     }
+    `);
+  });
+});
+
+describe('num', () => {
+  it('should match a number', () => {
+    const parser = num(12);
+
+    expect(run(parser, '12')).toMatchObject({
+      success: true,
+      value: 12,
+      remaining: '',
+      original: '12',
     });
 
-    expect(run(parser, '')).toMatchObject({
-      success: false,
-      error: expect.stringContaining(`"<end of input>"`),
-      original: '',
-    });
+    expect(run(parser, '130')).toMatchInlineSnapshot(`
+     {
+       "error": "Expected "12", but received "13"",
+       "original": "130",
+       "success": false,
+     }
+    `);
+
+    expect(run(parser, '')).toMatchInlineSnapshot(`
+     {
+       "error": "Expected "12", but received "<end of input>"",
+       "original": "",
+       "success": false,
+     }
+    `);
   });
 });
 
 describe('sequenceOf', () => {
   it('should process a sequence of parsers', () => {
-    const parser = sequenceOf([str('Hello'), str(' '), str('world!')]);
+    const parser = sequenceOf([str('Hello '), num(4), str(' world!')]);
 
-    expect(run(parser, 'Hello world!')).toMatchObject({
+    const result = run(parser, 'Hello 4 world!');
+
+    expect(result).toMatchObject({
       success: true,
-      value: ['Hello', ' ', 'world!'],
+      value: ['Hello ', 4, ' world!'],
       remaining: '',
-      original: 'Hello world!',
+      original: 'Hello 4 world!',
     });
+
+    expect(run(parser, 'Hello 5 world!')).toMatchInlineSnapshot(`
+     {
+       "error": "Expected "4", but received "5"",
+       "original": "5 world!",
+       "success": false,
+     }
+    `);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,27 @@ export const str =
     };
   };
 
+export const num =
+  (expected: number): Parser<number> =>
+  (input: Input) => {
+    const expectedStr = expected.toString();
+    if (input.startsWith(expectedStr)) {
+      return {
+        success: true,
+        value: expected,
+        remaining: input.slice(expectedStr.length),
+        original: input,
+      };
+    }
+
+    const received = input.slice(0, expectedStr.length) || '<end of input>';
+    return {
+      success: false,
+      error: `Expected "${expectedStr}", but received "${received}"`,
+      original: input,
+    };
+  };
+
 export const run = <A>(parser: Parser<A>, input: Input): ParseResult<A> => {
   return parser(input);
 };
